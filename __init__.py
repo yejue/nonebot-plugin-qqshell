6f5017519c3ff9a8bf698a9ef2e3d7df8e01a794
yejue <1145331931@qq.com>
2024-05-31 15:13:32 +0800

    [chore] 元信息更新

--- __init__.py
+++ __init__.py
@@ -2,17 +2,16 @@ from nonebot import require
 require("nonebot_plugin_htmlrender")
 
 from nonebot.plugin import PluginMetadata
+
 from .config import Config
 from .handler import shell_handler
 
 __plugin_meta__ = PluginMetadata(
-    name="QQShell: 执行任何你所能想到的命令",
-    description=(
-        "QQShell 能执行任何你所能想到的服务器命令\n"
-        "   >shell cd /\n"
-        "   >shell pwd\n"
-        "   >shell su\n"
-    ),
-    usage=">shell 你的指令",
+    name="QQShell",
+    description="QQShell 是一个类 WebSSH、XShell 的运行在机器人的 SSH Shell，在 QQ 中自由运行你的命令吧",
+    usage=">shell <你的指令>",
     config=Config,
+    homepage="https://github.com/yejue/nonebot-plugin-qqshell.git",
+    type="application",
+    supported_adapters={"~onebot.v11"}
 )
